76724d63edf259c75340d48564b8a1065da73a85
Cecile <cecile.honnorat@gmail.com>
2022-05-03 17:35:13 +0200

    first elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button'
+import Navigation from './Components/Navbar';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Navigation />
+      <Row className="content" style={{ marginTop: 35 }}>
+        <Col>
+          <h1>Make remote work</h1>
+          <p>Get your team in sync, no matter your location.
+            Streamline processes, create team rituals, and watch productivity soar.</p>
+          <Button variant="dark" size="lg" style={{ borderRadius: 15 }}>Learn more</Button>
+          <div style={{marginTop:55}}>
+            <img
+              src='./images/client-databiz.svg'
+              alt='logo-databiz'
+              style={{marginRight:30}}
+            />
+            <img
+              src='./images/client-audiophile.svg'
+              alt='logo-audiophile'
+              style={{marginRight:30}}
+            />
+            <img
+              src='./images/client-meet.svg'
+              alt='logo-meet'
+              style={{marginRight:30}}
+            />
+            <img
+              src='./images/client-maker.svg'
+              alt='logo-maker'
+              style={{marginRight:30}}
+            />
+          </div>
+        </Col>
+        <Col>
+          <img
+            src='./images/image-hero-desktop.png'
+            alt="homme avec ordi"
+            width='80%'
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Button, Container } from 'react-bootstrap';
+
+export default function Navigation() {
+
+    return (
+        <Navbar expand="xl">
+        <Container fluid>
+        <Navbar.Brand><img src='../images/logo.svg' alt="logo"/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto my-2 my-lg-0" style={{margin:15}}>
+        <NavDropdown title="Features" id="basic-nav-dropdown"style={{margin:15}}>
+          <NavDropdown.Item >Todo List</NavDropdown.Item>
+          <NavDropdown.Item >Calendar</NavDropdown.Item>
+          <NavDropdown.Item >Reminders</NavDropdown.Item>
+          <NavDropdown.Item >Planning</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Company" id="basic-nav-dropdown"style={{margin:15}}>
+          <NavDropdown.Item >History</NavDropdown.Item>
+          <NavDropdown.Item >Our Team</NavDropdown.Item>
+          <NavDropdown.Item >Blog</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Item style={{margin:15}}><Nav.Link>Careers</Nav.Link></Nav.Item>
+        <Nav.Item style={{margin:15}}><Nav.Link>About</Nav.Link></Nav.Item>
+        </Nav>
+        <Nav className="d-flex">
+        <Nav.Item style={{margin:15}}><Nav.Link>Login</Nav.Link></Nav.Item>
+        <Button variant="outline-dark" style={{margin:15, borderRadius:15}}>Register</Button>
+        </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+}
